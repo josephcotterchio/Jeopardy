74d8e1689e8a5ce542a77590b19956adfaa75e08
joecotterchio <joecotterchio@gmail.com>
2021-01-29 16:50:46 -0500

    first shell

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.tableValues = [
+      {
+        value: "$200",
+      },
+      {
+        value: "$400",
+      },
+      {
+        value: "$600",
+      },
+      {
+        value: "$800",
+      },
+      {
+        value: "$1000",
+      },
+    ];
+    this.state = {
+      hasData: false,
+      columns: [
+        {
+          Header: "Category 1",
+          accessor: "value",
+          id: "1",
+          headerStyle: {
+            marginTop: "0px",
+            height: "75px",
+          },
+          style: {
+            borderRadius: "5px",
+            borderColor: "black",
+          },
+        },
+        {
+          Header: "Category 2",
+          accessor: "value",
+        },
+        {
+          Header: "Category 3",
+          accessor: "value",
+        },
+        {
+          Header: "Category 4",
+          accessor: "value",
+        },
+        {
+          Header: "Category 5",
+          accessor: "value",
+        },
+        {
+          Header: "Category 6",
+          accessor: "value",
+        },
+      ],
+    };
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Not Jeopardy - but like it</h1>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
