1143ec8a6b645ddd07a7874482ef5659a3b98662
joecotterchio <joecotterchio@gmail.com>
2021-02-01 15:29:44 -0500

    data

---
+++ src/data.js
@@ -0,0 +1,13 @@
+const data = [
+  {
+    category: "Final Jeopardy",
+    questions: [
+      {
+        points: 10000,
+        question:
+          "For what purpose was HTML originally developer?",
+        answer: "So that scientists could share papers amongst each other.",
+      },
+    ],
+  },
+];
